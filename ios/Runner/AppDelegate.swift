fe9cb9cb59851a2f8fbd9f891f21696e0af5146f
hazajadded-wq <hazajadded@gmail.com>
2025-12-09 01:00:44 +0300

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,44 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    
+    let snapChannel = FlutterMethodChannel(
+        name: "snap_webview",
+        binaryMessenger: controller.binaryMessenger
+    )
+    
+    snapChannel.setMethodCallHandler { (call: FlutterMethodCall, result: @escaping FlutterResult) in
+        if call.method == "takeSnapshot" {
+            
+            guard let rootView = controller.view else {
+                result(FlutterError(code: "NO_VIEW", message: "Root view not found", details: nil))
+                return
+            }
+            
+            // أخذ صورة للشاشة بما فيها WebView
+            UIGraphicsBeginImageContextWithOptions(
+                rootView.bounds.size,
+                false,
+                UIScreen.main.scale
+            )
+            
+            rootView.drawHierarchy(in: rootView.bounds, afterScreenUpdates: true)
+            let image = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            
+            if let uiImage = image, let data = uiImage.pngData() {
+                result(data)
+            } else {
+                result(FlutterError(code: "SNAP_ERROR", message: "Snapshot failed", details: nil))
+            }
+            
+        } else {
+            result(FlutterMethodNotImplemented)
+        }
+    }
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
