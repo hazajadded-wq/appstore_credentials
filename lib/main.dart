241d2da9c7d4a84a915a5e5008dfc2f56e2ccd64
Ali Mahmoud Ahmed <hazajadded@gmail.com>
2025-12-17 19:41:41 +0300

    Initial commit - Flutter app v1.0.11

--- lib/main.dart
+++ lib/main.dart
@@ -2059,6 +2059,8 @@ class _WebViewScreenState extends State<WebViewScreen> {
       controller = WebViewController()
         ..setJavaScriptMode(JavaScriptMode.unrestricted)
         ..setBackgroundColor(Colors.white)
+        ..setUserAgent(
+            'Mozilla/5.0 (iPhone; CPU iPhone OS 15_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6 Mobile/15E148 Safari/604.1')
         ..addJavaScriptChannel(
           'FlutterChannel',
           onMessageReceived: (JavaScriptMessage message) {
